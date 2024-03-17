f2894c017286f3f8e1ebd174bedf8c45c27f5876
Mohd-Salman-0119 <salmanansari910550@gmail.com>
2024-03-17 12:51:18 +0530

    remove: redis

--- imports/config.imports.js
+++ imports/config.imports.js
@@ -1,9 +1,7 @@
 const { connectDB } = require('../configs/db');
 const { generateToken } = require('../configs/generateToken');
-const { redisClient } = require('../configs/redisClient');
 
 module.exports = {
      connectDB,
      generateToken,
-     redisClient
 };
